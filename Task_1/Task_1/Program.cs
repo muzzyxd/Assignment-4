9bc65b3582c41e2f89e0f16006a88e4c2af6d8f2
muzzyxd <sheikhmuzammil676@gmail.com>
2024-11-13 16:11:10 +0500

    Assignment 3

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        public class Animal
+        {
+         
+            protected string name;
+            protected int age;
+
+            
+            public Animal(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+          
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Name: {name}, Age: {age}");
+            }
+        }
+
+        
+        public class Dog : Animal
+        {
+            
+            public Dog(string name, int age) : base(name, age) { }
+
+            
+            public void Bark()
+            {
+                Console.WriteLine("Woof!");
+            }
+        }
+
+        
+        public class Cat : Animal
+        {
+            
+            public Cat(string name, int age) : base(name, age) { }
+
+            
+            public void Meow()
+            {
+                Console.WriteLine("Meow!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Buddy", 5);
+            dog.DisplayInfo(); 
+            dog.Bark();        
+
+            
+            Cat cat = new Cat("Munchkin", 3);
+            cat.DisplayInfo(); 
+            cat.Meow();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task_2/Task_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        public class Person
+        {
+         
+            protected string firstName;
+            protected string lastName;
+
+            public Person(string firstName, string lastName)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+            }
+
+           
+            public string GetFullName()
+            {
+                return $"{firstName} {lastName}";
+            }
+        }
+
+       
+        public class Employee : Person
+        {
+            
+            private string employeeId;
+
+           
+            public Employee(string firstName, string lastName, string employeeId)
+                : base(firstName, lastName)
+            {
+                this.employeeId = employeeId;
+            }
+
+    
+            public void DisplayEmployeeInfo()
+            {
+                Console.WriteLine($"Full Name: {GetFullName()}, Employee ID: {employeeId}");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Employee employee1 = new Employee("Ahmed", "Khan", "1001");
+            employee1.DisplayEmployeeInfo();
+
+            Employee employee2 = new Employee("Abdul", "Rehman", "1002");
+            employee2.DisplayEmployeeInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
